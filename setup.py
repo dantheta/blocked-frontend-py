a20e9c01e90cc7b288bc86b641303cd6bb1d97f9
Daniel Ramsay <daniel@dretzq.org.uk>
2017-06-05 11:47:30 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="BlockedFrontend",
+    version='2.0.0',
+    description="A flask-based frontend for blocked.org.uk",
+    url="https://www.blocked.org.uk",
+    author="Daniel Ramsay",
+    author_email="daniel@dretzq.org.uk",
+    license="GPL",
+    keywords="blocked censorship",
+    packages=["BlockedFrontend"],
+    install_requires=[
+        'flask>=0.10',
+        'requests>=2.9'
+        ]
+)
+    
