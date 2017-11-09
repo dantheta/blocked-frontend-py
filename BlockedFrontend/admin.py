0e2369045983dd9e6ac404a9b61b982e69bf60e2
Daniel Ramsay <daniel@dretzq.org.uk>
2017-11-09 22:36:30 +0000

    merge list functionality; openrightsgroup/blocked-org-uk#176

--- BlockedFrontend/admin.py
+++ BlockedFrontend/admin.py
@@ -1,10 +1,13 @@
 
+import logging
 
 from flask import Blueprint, render_template, redirect, request, \
-    g, url_for, abort, config, current_app, session
+    g, url_for, abort, config, current_app, session, flash
 
 from models import *
 from auth import *
+
+from NORM.exceptions import ObjectNotFound,ObjectExists
 
 admin_pages = Blueprint('admin', __name__, template_folder='templates/admin')
 
@@ -80,3 +83,44 @@ def savedlist_frontpage(id, state):
     savedlist.store()
     request.conn.commit()
     return redirect(url_for('.savedlists'))
+
+@admin_pages.route('/control/savedlists/merge', methods=['POST'])
+@check_admin
+def savedlist_merge():
+    ids = request.form.getlist('merge')
+    logging.info("Merge: %s", ids)
+    if not isinstance(ids, list):
+        abort(500)
+    ids.sort()
+    first = SavedList(request.conn, id=ids[0])
+    c = request.conn.cursor()
+    for id in ids[1:]:
+        # try to update in bulk
+        c.execute("savepoint point1")
+        try:
+            c.execute("update items set list_id = %s where list_id = %s",
+                [first['id'], id])
+        except psycopg2.IntegrityError:
+            c.execute("rollback to point1")
+        else:
+            # bulk move succeeded, onto the next list
+            SavedList(request.conn, id=id).delete()
+            continue
+
+        # otherwise, move one-by-one and delete on duplicate
+        for item in Item.select(request.conn, list_id = id):
+            try:
+                c.execute("savepoint point1")
+                item['list_id'] = first['id']
+                item.store()
+            except ObjectExists:
+                c.execute("rollback to point1")
+                item.delete()
+        SavedList(request.conn, id=id).delete()
+    request.conn.commit()
+    flash("Selected lists have been merged into '{}'".format(first['name']))
+    return redirect(url_for('.savedlists'))
+                
+
+
+
--- BlockedFrontend/db.py
+++ BlockedFrontend/db.py
@@ -1,8 +1,12 @@
 
 from flask import current_app
+from psycopg2.extras import DictCursor
 import psycopg2
 
-__all__ = ['db_connect']
+__all__ = ['db_connect','cursor']
 
 def db_connect():
-    return psycopg2.connect(current_app.config['DB'])
+    return psycopg2.connect(current_app.config['DB'], cursor_factory=DictCursor)
+
+def cursor(conn):
+    c = conn.cursor(cursor_factory = DictCursor)
