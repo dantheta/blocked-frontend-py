0eb27aec012d7e4c5e55c80ed3ffb5bc497f3f89
Daniel Ramsay <daniel@dretzq.org.uk>
2018-02-08 02:04:26 +0000

    test case editor; openrightsgroup/blocked-org-uk#316

--- BlockedFrontend/resources.py
+++ BlockedFrontend/resources.py
@@ -1,8 +1,14 @@
 import yaml
 from flask import current_app
 
-__all__ = ['load_country_data']
+__all__ = ['load_country_data','load_isp_data','load_data']
+
+def load_data(filename):
+    with current_app.open_resource('data/'+filename+'.yml') as fp:
+        return yaml.load(fp)
 
 def load_country_data():
-    with current_app.open_resource('data/countries.yml') as fp:
-        return yaml.load(fp)
+    return load_data('countries')
+
+def load_isp_data():
+    return load_data('isps')
