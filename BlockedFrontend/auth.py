f5af7656fc0fce93cb457417451a0074af63acdd
Daniel Ramsay <daniel@dretzq.org.uk>
2020-10-04 16:06:08 +0100

    move ispreports into separate module

--- BlockedFrontend/auth.py
+++ BlockedFrontend/auth.py
@@ -2,7 +2,7 @@
 from flask import g, abort
 from functools import wraps
 
-__all__ = ['AdminPermissionException','check_admin','check_moderator']
+__all__ = ['AdminPermissionException','check_admin','check_moderator', 'check_reviewer']
 
 LEVELS = ['user','reviewer','moderator','admin']
 
@@ -18,6 +18,7 @@ def level_compare(current, required):
 
 # decorator to check admin
 
+
 def check_admin(s):
     @wraps(s)
     def wrapped(*args, **kw):
@@ -25,10 +26,19 @@ def check_admin(s):
         return s(*args, **kw)
     return wrapped
 
+
 def check_moderator(s):
     @wraps(s)
     def wrapped(*args, **kw):
         check_level('moderator')
+        return s(*args, **kw)
+    return wrapped
+
+
+def check_reviewer(s):
+    @wraps(s)
+    def wrapped(*args, **kw):
+        check_level('reviewer')
         return s(*args, **kw)
     return wrapped
 
--- BlockedFrontend/server.py
+++ BlockedFrontend/server.py
@@ -46,6 +46,9 @@ if app.config['MODULE_ADMIN']:
     from admin_savedlists import admin_savedlist_pages
     app.register_blueprint(admin_savedlist_pages)
 
+    from admin_ispreports import admin_ispreport_pages
+    app.register_blueprint(admin_ispreport_pages)
+
 if app.config.get('SITE_THEME') == '451':
     from err451 import err451_pages
     app.register_blueprint(err451_pages)
