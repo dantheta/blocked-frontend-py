5d52fc946abce74de3d1e864717804f128540c6e
Daniel Ramsay <daniel@dretzq.org.uk>
2021-04-05 19:52:01 +0100

    Remove registry suspensions page openrightsgroup/Blocking-Middleware#94

--- BlockedFrontend/registry.py
+++ BlockedFrontend/registry.py
@@ -1,18 +1,9 @@
-import os
-import re
-import psycopg2
-import logging
-import datetime
-import itertools
 
 from flask import Blueprint, render_template, redirect, request, \
     g, url_for, abort, config, current_app, session, flash, jsonify, Response
 
 from models import *
-from auth import *
 from utils import *
-from resources import *
-from db import *
 
 import NORM
 from NORM.exceptions import ObjectNotFound,ObjectExists
@@ -21,33 +12,6 @@ registry_pages = Blueprint('registry', __name__, template_folder='templates/regi
 
 
 @registry_pages.route('/')
-@registry_pages.route('/suspensions')
-@registry_pages.route('/suspensions/<int:page>')
-def registry_suspensions(page=1):
-    g.remote_content = g.remote.get_content('registry_suspensions')
-
-    pagesize = 25
-    offset = (page-1)*pagesize
-
-    res = NORM.Query(g.conn, "select count(*), max(created) from public.registry_suspension_urls", [])
-    row = res.fetchone()
-    count = row[0]
-    pagecount = get_pagecount(count, pagesize)
-    newdate = row[1].date()
-
-    sql = "select * from public.registry_suspension_urls limit {0} offset {1}".format(pagesize, offset)
-    print sql
-    suspensions = NORM.Query(g.conn, sql, [])
-    g.conn.commit()
-    return render_template('registry_suspensions.html',
-                           suspensions=suspensions,
-                           page=page,
-                           count=count,
-                           newdate=newdate,
-                           pagesize=pagesize,
-                           pagecount=pagecount)
-
-
 @registry_pages.route('/seizures')
 @registry_pages.route('/seizures/<int:page>')
 def registry_seizures(page=1):
@@ -57,7 +21,7 @@ def registry_seizures(page=1):
     offset = (page-1)*pagesize
 
     res = NORM.Query(g.conn,
-                     "select count(*), max(created) from public.url_latest_status "
+                     "select count(distinct urlid), max(created) from public.url_latest_status "
                      "where blocktype = 'SUSPENSION'",
                      [])
     row = res.fetchone()
@@ -65,12 +29,13 @@ def registry_seizures(page=1):
     newdate = newdate.date()
     pagecount = get_pagecount(count, pagesize)
 
-    sql = ("select url, uls.created, uls.last_blocked, isps.description isp_description "
+    sql = ("select url, max(uls.created) created, max(uls.last_blocked) last_blocked, string_agg(isps.description, ';') isp_description "
            "from url_latest_status uls "
            "inner join urls using (urlid) "
            "inner join isps on network_name = isps.name "
            "where blocktype = 'SUSPENSION' "
-           "order by created desc "
+           "group by url "
+           "order by created desc, url "
            "limit {0} offset {1}".format(pagesize, offset))
     q = NORM.Query(g.conn, sql, [])
     g.conn.commit()
