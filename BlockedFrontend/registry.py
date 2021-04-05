1f5e6c7b355e5744ed9ad41df604d803500fc9a6
Daniel Ramsay <daniel@dretzq.org.uk>
2021-04-05 19:36:31 +0100

    Updated registry suspensions page openrightsgroup/Blocking-Middleware#94

--- BlockedFrontend/registry.py
+++ BlockedFrontend/registry.py
@@ -24,6 +24,8 @@ registry_pages = Blueprint('registry', __name__, template_folder='templates/regi
 @registry_pages.route('/suspensions')
 @registry_pages.route('/suspensions/<int:page>')
 def registry_suspensions(page=1):
+    g.remote_content = g.remote.get_content('registry_suspensions')
+
     pagesize = 25
     offset = (page-1)*pagesize
 
@@ -31,7 +33,7 @@ def registry_suspensions(page=1):
     row = res.fetchone()
     count = row[0]
     pagecount = get_pagecount(count, pagesize)
-    newdate = row[1]
+    newdate = row[1].date()
 
     sql = "select * from public.registry_suspension_urls limit {0} offset {1}".format(pagesize, offset)
     print sql
@@ -44,3 +46,39 @@ def registry_suspensions(page=1):
                            newdate=newdate,
                            pagesize=pagesize,
                            pagecount=pagecount)
+
+
+@registry_pages.route('/seizures')
+@registry_pages.route('/seizures/<int:page>')
+def registry_seizures(page=1):
+    g.remote_content = g.remote.get_content('registry_seizures')
+
+    pagesize = 25
+    offset = (page-1)*pagesize
+
+    res = NORM.Query(g.conn,
+                     "select count(*), max(created) from public.url_latest_status "
+                     "where blocktype = 'SUSPENSION'",
+                     [])
+    row = res.fetchone()
+    (count, newdate) = row
+    newdate = newdate.date()
+    pagecount = get_pagecount(count, pagesize)
+
+    sql = ("select url, uls.created, uls.last_blocked, isps.description isp_description "
+           "from url_latest_status uls "
+           "inner join urls using (urlid) "
+           "inner join isps on network_name = isps.name "
+           "where blocktype = 'SUSPENSION' "
+           "order by created desc "
+           "limit {0} offset {1}".format(pagesize, offset))
+    q = NORM.Query(g.conn, sql, [])
+    g.conn.commit()
+
+    return render_template("registry_seizures.html",
+                           seizures=q,
+                           page=page,
+                           count=count,
+                           pagecount=pagecount,
+                           pagesize=pagesize,
+                           newdate=newdate)
