0a059c3c70b67d401080ad53e6117e16ed05ed59
Daniel Ramsay <daniel@dretzq.org.uk>
2024-10-31 11:55:39 +0000

    file mode

--- BlockedFrontend/resources.py
+++ BlockedFrontend/resources.py
@@ -5,11 +5,11 @@ from flask import current_app
 __all__ = ['load_country_data','load_isp_data','load_data','load_csv']
 
 def load_data(filename):
-    with current_app.open_resource('data/'+filename+'.yml') as fp:
+    with current_app.open_resource('data/'+filename+'.yml', 'r') as fp:
         return yaml.safe_load(fp)
 
 def load_csv(filename):
-    with current_app.open_resource('data/'+filename+'.csv') as fp:
+    with current_app.open_resource('data/'+filename+'.csv', 'r') as fp:
         reader = csv.reader(fp)
         for row in reader:
             yield row
